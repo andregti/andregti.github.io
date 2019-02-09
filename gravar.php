ce3556fc57dedb63d0c6e72d41d0e3ac603895c5
André Pereira <43019680+andregti@users.noreply.github.com>
2019-02-08 22:05:12 -0300

    Update gravar.php

--- gravar.php
+++ gravar.php
@@ -2,17 +2,17 @@
 
 $email = $_POST['email'];
 
-$conteudo = "$email\r\n";
+$conteudo = "$email";
 
 $arquivo = "dados.txt";
 
-$criar = fopen($arquivo, "a");
+$criar = fopen($arquivo, "a+");
 
 $escrever = fwrite($criar, $conteudo);
 
 echo "Arquivo gravado com Sucesso!";
 
-// if($escrever == true){ echo "Dados armazenados em $arquivo"; } else { echo "Erro ao salvar dados em $arquivo"; 
+if($escrever == true){ echo "Dados armazenados em $arquivo"; } else { echo "Erro ao salvar dados em $arquivo"; 
 
 fclose($abrir);
 
